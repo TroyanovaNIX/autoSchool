bcefbde6070eb741f376c5f688428e863cb750e8
troyanova <yevgeniya.troyanova@nixsolutions.com>
2019-07-22 10:46:26 +0300

    Update ProductPage with select

--- NewProjectSchoolAutomation/src/Module11/main/java/ProductPage.java
+++ NewProjectSchoolAutomation/src/Module11/main/java/ProductPage.java
@@ -2,11 +2,18 @@ package Module11.main.java;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
 
 public class ProductPage extends Page {
     public By title = new By.ByCssSelector("span#productTitle");
     public By price = new By.ByXPath("//div [contains(@class,'a-section a-spacing-none a-padding-none')]//span[contains(@class,'a-color-price')]");
     public By addToCartButton = new By.ByXPath("//input[contains(@id,'add-to-cart-button')]");
+    public By dropdown = new By.ByCssSelector("#native_dropdown_selected_size_name.a-native-dropdown");
 
     public ProductPage(WebDriver driver) {
         super(driver);
@@ -17,6 +24,18 @@ public class ProductPage extends Page {
     }
 
     public String getPrice() {
+        if (isElementPresent(dropdown)) {
+            Select select = new Select(driver.findElement(dropdown));
+            List<WebElement> selectList = select.getOptions();
+            for (int i = 1; i < selectList.size(); i++) {
+                if (selectList.get(i).isEnabled()) {
+                    select.selectByIndex(i);
+                    break;
+                }
+            }
+        }
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+        WebElement productPrice = wait.until(ExpectedConditions.visibilityOfElementLocated(price));
         String currentPrice = driver.findElement(price).getText();
         String realPrice = currentPrice.substring(1);
         return realPrice;
@@ -28,3 +47,4 @@ public class ProductPage extends Page {
     }
 
 }
+
